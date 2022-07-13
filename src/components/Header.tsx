a87092fdfa85bacbd3429e77c8d9d53ad44083d1
FelipeKonig <felipe.konig@compasso.com.br>
2022-07-13 00:32:40 -0300

    header added

---
+++ src/components/Header.tsx
@@ -0,0 +1,15 @@
+import styles from "./Header.module.css";
+
+import rocketLogo from '/assets/rocket-logo.svg';
+import todoLogo from '/assets/todo-logo.svg';
+
+export function Header() {
+    return (
+        <header className={styles.header} >
+            <div className={styles.logo}>
+                <img className={styles.rocket} src={rocketLogo} alt="logo com um foguete com borda azul" />
+                <img  className={styles.todo} src={todoLogo} alt="logo escrito 'TODO' sem azul e roxo" />
+            </div>
+        </header>
+    )
+}
