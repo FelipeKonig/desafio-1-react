f779667b20c385179718beb93c0f6ac9194b1361
FelipeKonig <felipe.konig@compasso.com.br>
2022-07-13 00:53:15 -0300

    fix the logo

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -6,10 +6,13 @@ import todoLogo from '/assets/todo-logo.svg';
 export function Header() {
     return (
         <header className={styles.header} >
-            <div className={styles.logo}>
-                <img className={styles.rocket} src={rocketLogo} alt="logo com um foguete com borda azul" />
-                <img  className={styles.todo} src={todoLogo} alt="logo escrito 'TODO' sem azul e roxo" />
+            <div className={styles.centerLogo}>
+                <div className={styles.logo}>
+                    <img className={styles.rocket} src={rocketLogo} alt="logo com um foguete com borda azul" />
+                    <img  className={styles.todo} src={todoLogo} alt="logo escrito 'TODO' sem azul e roxo" />
+                </div>
             </div>
+           
         </header>
     )
 }
