5e599113c6407e73dd5d6c3f172c9406479865c0
FelipeKonig <felipe.konig@compasso.com.br>
2022-07-19 04:17:40 -0300

    responsive list with tasks  added

--- src/components/List.tsx
+++ src/components/List.tsx
@@ -1,6 +1,8 @@
+import { Circle } from 'phosphor-react';
 import styles from './List.module.css';
 
 import clipboardLogo from '/assets/clipboard-logo.svg';
+import trashLogo from '/assets/trash-logo.svg';
 
 export function List() {
     return (
@@ -20,18 +22,27 @@ export function List() {
                         Concluídas
                         <div className={styles.border}>
                             <span className={styles.number}>
-                                0
+                                2
                             </span>
                         </div>
                     </span>
                 </div>
             </div>
-            <div className={styles.empty}>
-                <img src={clipboardLogo} alt="a logo de uma prancheta" />
-                <span className={styles.emptyList}>
-                    Você ainda não tem tarefas cadastradas <br />
-                    Crie tarefas e organize seus itens a fazer
-                </span>
+            <div className={styles.listItems}>
+                    {/* <img src={clipboardLogo} alt="a logo de uma prancheta" />
+                    <span className={styles.emptyList}>
+                        Você ainda não tem tarefas cadastradas <br />
+                        Crie tarefas e organize seus itens a fazer
+                    </span> */}
+                <div className={styles.task}>
+                    <div className={styles.check}>
+                        <Circle size={24} className={styles.icon}/>
+                    </div>
+                    <p className={styles.contentTask}>
+                        Integer urna interdum massa libero auctor neque turpis turpis semper. Duis vel sed fames integer.
+                    </p>
+                    <img className={styles.trash} src={trashLogo} alt="o icone de um lixeiro em cinza" />
+                </div>
             </div>
         </div>
     )
--- src/components/NewTask.tsx
+++ src/components/NewTask.tsx
@@ -6,7 +6,7 @@ export function NewTask() {
     return (
         <form className={styles.principal} action="">
             <input type="text" placeholder="Adicione uma nova palavra"/>
-            <button>Criar <img src={ addLogo } alt="icone com um botao de mais"/></button>
+            <button><span>Criar</span> <img src={ addLogo } alt="icone com um botao de mais"/></button>
         </form>
     )
 }
